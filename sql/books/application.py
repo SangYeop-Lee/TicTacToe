f2829554c760c36cb98903e6b46b74c110b2b42c
sangyeop-lee <ubuntu@ide.ide.fa8a668f-66bf-41ef-8858-10c2a9e6af38.svc.cluster.local>
2019-12-01 12:01:40 +0000

    TODO -sql

---
+++ sql/books/application.py
@@ -0,0 +1,6 @@
+# TODO
+# books.csv = {ispn, name, year}
+# create heroku database
+# decide schemas (authors, books, etc..)
+# import data into database
+# write python program that queries the database for book by its (partial) title or author
