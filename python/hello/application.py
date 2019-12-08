5fba7c0832bc8b1f287848ed2d4e5edff5edfb8d
sangyeop-lee <ubuntu@ide.ide.fa8a668f-66bf-41ef-8858-10c2a9e6af38.svc.cluster.local>
2019-12-08 15:03:32 +0000

    react flashcards

---
+++ python/hello/application.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Hello, world!"
+
+# <type:varName>
+@app.route("/<string:name1>/<string:name2>")
+@app.route("/<string:name1>")
+def hello(name1, name2="Josh"):
+    name1 = name1.capitalize()
+    name2 = name2.capitalize()
+    return f"Hello, {name1}, {name2}!"
--- python/tictactoe/ai.py
+++ python/tictactoe/ai.py
@@ -4,9 +4,10 @@ board = [[None, None, None], [None, None, None], [None, None, None]]
 turn = "X"
 
 class node:
-    def __init__(self, board, turn):
+    def __init__(self, board, turn, winner):
         self.board = board
         self.turn = turn
+        self.winner = winner
 
     def search(self):
         pass
