9d5dbaa220b563b134ed57097742878b6a21571e
sangyeop-lee <ubuntu@ide.ide.fa8a668f-66bf-41ef-8858-10c2a9e6af38.svc.cluster.local>
2020-01-02 14:36:24 +0000

    database structure

--- project/application.py
+++ project/application.py
@@ -5,3 +5,11 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/calendar")
+def calendar():
+    return render_template("calendar.html")
+
+@app.route("/diary")
+def diary():
+    return render_template("diary.html")
